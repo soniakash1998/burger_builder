ec3e4e8a630a757c11ad3cc3d2296fb248999a31
Akash Soni <akashsoninit16@gmail.com>
2019-03-04 11:05:04 +0000

    added responsive sidedrawer and made toolbar more responsive

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Layout.module.css';
 import Aux from '../../HOC/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <main className = {classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
-
+class Layout extends Component{
+    
+    state = {
+        showSideDrawer: false
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer : false});
+    }
+    
+    sideDrawerToggleHandler = (prevState) =>{
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        })
+    } 
+    
+    render(){
+        return(
+            <Aux>
+                <Toolbar drawerToggleClicked = {this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    open = {this.state.showSideDrawer}
+                    closed = {this.sideDrawerClosedHandler}/>
+                <main className = {classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>        
+        );
+    }
+    
+}
 export default Layout;
